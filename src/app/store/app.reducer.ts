16db456a4c448d4f9a4fb29c32ee9b6f795b2dea
JecaTatu <gas5@cin.ufpe.br>
2020-12-21 16:43:50 -0300

    adds menu store to app reducer

--- src/app/store/app.reducer.ts
+++ src/app/store/app.reducer.ts
@@ -1,10 +1,13 @@
-import { ActionReducerMap } from '@ngrx/store';
 import { routerReducer, RouterReducerState } from '@ngrx/router-store';
+import { ActionReducerMap } from '@ngrx/store';
+import { MenuState, menuReducer } from './menu/menu.reducer';
 
 export interface AppState {
   router: RouterReducerState;
+  menu: MenuState
 }
 
 export const reducers: ActionReducerMap<AppState> = {
   router: routerReducer,
+  menu: menuReducer
 };

d12086cd39112b8850673105b11e72a39f6b3803
JecaTatu <gas5@cin.ufpe.br>
2020-12-21 16:23:23 -0300

    installs ngrx

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HttpClientModule } from '@angular/common/http';
+import { StoreModule } from '@ngrx/store';
+import { EffectsModule } from '@ngrx/effects';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { StoreRouterConnectingModule } from '@ngrx/router-store';
+
+
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [
@@ -23,7 +30,13 @@ import { HttpClientModule } from '@angular/common/http';
     MaterialModule,
     AuthRoutingModule,
     HttpClientModule,
-
+    StoreModule.forRoot({}, {}),
+    EffectsModule,
+    StoreRouterConnectingModule,
+    StoreDevtoolsModule.instrument({
+      maxAge: 25, // Retains last 25 states
+      logOnly: environment.production // Restrict extension to log-only mode
+    }),
   ],
   exports:[
     MaterialModule

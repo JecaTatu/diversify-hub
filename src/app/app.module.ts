aefafd8951c42a2155217e856a7c554b3a3c29bc
JecaTatu <gas5@cin.ufpe.br>
2020-12-21 16:42:10 -0300

    adds reducers to for root of sotre in app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,21 @@
+import { reducers } from './store/app.reducer';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientModule } from '@angular/common/http';
+
+import { StoreModule } from '@ngrx/store';
+import { EffectsModule } from '@ngrx/effects';
+import { StoreRouterConnectingModule } from '@ngrx/router-store';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+
+import { ToastrModule } from 'ngx-toastr';
+
 import { AuthRoutingModule } from './auth/auth-routing.module';
 import { MaterialModule } from './shared/material.module';
 import { AuthModule } from './auth/auth.module';
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { ToastrModule } from 'ngx-toastr';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { HttpClientModule } from '@angular/common/http';
-import { StoreModule } from '@ngrx/store';
-import { EffectsModule } from '@ngrx/effects';
-import { StoreDevtoolsModule } from '@ngrx/store-devtools';
-import { StoreRouterConnectingModule } from '@ngrx/router-store';
-
-
 import { environment } from '../environments/environment';
 
 @NgModule({
@@ -30,7 +31,7 @@ import { environment } from '../environments/environment';
     MaterialModule,
     AuthRoutingModule,
     HttpClientModule,
-    StoreModule.forRoot({}, {}),
+    StoreModule.forRoot(reducers),
     EffectsModule,
     StoreRouterConnectingModule,
     StoreDevtoolsModule.instrument({
